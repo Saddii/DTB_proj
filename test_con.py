a27b62e5c3a196fb60585721fedfd84a4968a9a5
Saddii <domekzos01@gmail.com>
2024-05-12 16:41:01 +0200

    delete test

--- test_con.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-con = mysql.connector.connect(
-            host = "127.0.0.1",
-            user = "root",
-            password = "password",
-            database = "clients_base",
-            port= 3307
-            )
